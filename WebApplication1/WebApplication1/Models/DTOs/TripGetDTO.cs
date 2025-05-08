b4ed09a926d6727f21b167e79911e2a33d2b4c20
s32080 <s32080@pjwstk.edu.pl>
2025-05-08 23:10:16 +0200

    Beginning

---
+++ WebApplication1/WebApplication1/Models/DTOs/TripGetDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Controllers.DTOs;
+
+public class TripGetDTO
+{
+    public int IdTrip { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+    public int MaxPeople { get; set; }
+    
+}
